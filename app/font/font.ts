08be883507c8c1eb3ea60040a7498c44becccba7
daniherreraa <herreraacevedodl@gmail.com>
2024-07-03 01:01:19 -0500

    first commit

---
+++ app/font/font.ts
@@ -0,0 +1,13 @@
+import { Syne, Space_Grotesk } from "next/font/google";
+
+export const syne = Syne({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-syne'
+});
+
+export const spaceGrotesk = Space_Grotesk({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-spaceGrotesk'
+})
